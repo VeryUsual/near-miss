5587a2ff84da267dd1db28a6aa4c715b0320083b
ProgramminCat <72707293+ProgramminCat@users.noreply.github.com>
2026-02-04 08:10:34 -0500

    misc things

---
+++ README.md
@@ -0,0 +1,24 @@
+# Near Miss
+
+## Play
+Play the game [here](https://todo.todo/todo) on itch.io.
+
+## Controls
+WASD to Move
+
+Left Shift to Sprint
+
+F to Open Card Deck
+
+## Card Deck
+Press F to open your Card Deck. In there, you can activate abilities that help you survive longer.
+
+<br><br>
+<hr>
+<br>
+
+Created by me, all art drawn by me, music and sfx from Pixabay, and fonts from Kenney.
+
+Built using the Godot Game Engine.
+
+Licensed under the GNU General Public License 3.0.
--- Scripts/game_over.gd
+++ Scripts/game_over.gd
@@ -1,10 +1,15 @@
 extends Control
 
+var last_game_duration: int = 0: set = _on_last_game_dur_set
+var fake_tweened_last_game_duration: int = 0: set = _on_fake_tweened_last_game_duration_set
+var tween: Tween
+
 func _ready() -> void:
 	Engine.time_scale = 1.0
 	$PBLabel.hide()
 	
-	$Label2.text = "You survived for " + str(snapped(Globals.last_game_duration, 0.01)) + " seconds."
+	last_game_duration = snapped(Globals.last_game_duration, 0.01)
+	
 	if Globals.last_game_duration > Globals.personal_best:
 		Globals.personal_best = Globals.last_game_duration
 		$PBLabel.text = "You set a NEW personal best!"
@@ -12,8 +17,24 @@ func _ready() -> void:
 		$PBLabel.text = "Personal Best: " + str(snapped(Globals.personal_best, 0.01)) + " seconds."
 	$PBLabel.show()
 
+func _on_last_game_dur_set(new_value: int) -> void:
+	last_game_duration = new_value
+	
+	tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUAD)
+	if last_game_duration >= 5.0:
+		if last_game_duration <= 10.0:
+			tween.tween_property(self, "fake_tweened_last_game_duration", last_game_duration, 1.0)
+		else:
+			tween.tween_property(self, "fake_tweened_last_game_duration", last_game_duration, 2.0)
+	else:
+		fake_tweened_last_game_duration = last_game_duration
+
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
 
 func _on_button_2_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/game.tscn")
+
+func _on_fake_tweened_last_game_duration_set(new_value: int) -> void:
+	fake_tweened_last_game_duration = new_value
+	$Label2.text = "You survived for " + str(snapped(fake_tweened_last_game_duration, 0.01)) + " seconds."
